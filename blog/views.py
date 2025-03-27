c00874f5e546f06bdf7fd457a1dcc6a5a9b807cd
Artur23041989 <danh1989@mail.ru>
2025-03-27 21:47:27 +0300

    new file django

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .models import Post
+from .forms import PostForm
 
 
 def index(request):
-    context = {"title": "Главная страница"}
+    posts = Post.objects.all() # получение всех постов (select * from blog_post)
+
+    context = {"title": "Главная страница", "posts": posts}
     return render(request, template_name='blog/index.html', context=context)
 
 def about(request):
     context = {"title": "О сайте"}
     return render(request, template_name='blog/index.html', context=context)
 
+def add_post(request):
+    post_form = PostForm
+    context = {'form': post_form}
+    return render(request, template_name='blog/post_add.html', context=context)
 
 
 

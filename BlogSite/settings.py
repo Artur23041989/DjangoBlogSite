39697aec0ce101ac231c1448684181c601f0a0c8
Artur23041989 <danh1989@mail.ru>
2025-04-08 21:31:08 +0300

    del post

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-01wiw=cs^yan(ubx*%h@**idy@+pag$(24q@90of%3uyg*a7(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- BlogSite/urls.py
+++ BlogSite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from BlogSite import settings
@@ -23,5 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "blog.views.page_not_found"
+handler403 = "blog.views.forbidden"
+handler500 = "blog.views.server_error"
